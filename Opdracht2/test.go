0184ec7c5e38d89d390ddec3d36031ab14ac0ada
Joep den Ouden <538290@student.fontys.nl>
2024-09-15 21:42:24 +0200

    Opdracht 2 toegevoegd

--- Opdracht2/test.go
+++ Opdracht2/test.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"os"
+)
 
-var distanceKm int
-var rotationHours int
+var distanceM int
+var rotationHours float64
 
 func main() {
 	asknumbers()
@@ -12,13 +16,20 @@ func main() {
 }
 
 func speedcalculation() {
-	fmt.Println(distanceKm * rotationHours)
+	var circumference float64 = 2 * math.Pi * float64(distanceM)
+	speed := circumference / rotationHours
+
+	result := fmt.Sprintf("The speed of the planet is: %.2f km/h\n", speed)
+
+	saveToFile(result)
+
+	fmt.Print(result)
 }
 
 func asknumbers() {
 
 	fmt.Println("What is the km distance of the planet from the sun? (in Millions)")
-	_, err := fmt.Scanln(&distanceKm)
+	_, err := fmt.Scanln(&distanceM)
 	if err != nil {
 		fmt.Println("Error reading the first number. Please ensure you enter a valid number.")
 		return
@@ -31,4 +42,17 @@ func asknumbers() {
 		return
 	}
 
+}
+
+func saveToFile(data string) {
+	file, err := os.Create("result.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(data)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+	}
 }
